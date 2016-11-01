b05082c2ca3d8d54be7c4f4a6b52dea742064c35
Alexander <alexander.lock7@gmail.com>
2016-11-01 15:47:00 +0000

    test props and state with loading timer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,41 @@ import React, {Component} from 'react';
 import MessageList from './MessageList.jsx';
 import MessageBar from './MessageBar.jsx';
 
+var data = {
+  currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+  messages: [
+    {
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
 
+    },
+    {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ],
+};
 
 class App extends Component {
+
   constructor(props){
-    super (props);
+     super (props);
+    this.state = {loading:true};
+
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false})  // change the state. this calls render() and the component updates.
+    }, 3000)
   }
 
   render() {
     console.log("Rendering <App/>");
+
+    if(this.state.loading){
+      return <h1>loading...</h1>
+    } else {
     return (
       <div className ="wrapper">
         <nav>
@@ -19,7 +45,7 @@ class App extends Component {
         <MessageList/>
         <MessageBar/>
       </div>
-    );
+    ); }
   }
 }
 export default App;
